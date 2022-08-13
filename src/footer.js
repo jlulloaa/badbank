55fe3a0795a2572edffd33fa03c6e1c35b1a1985
Jose L. Ulloa <jose.ulloa@isandex.com>
2022-08-13 11:56:49 -0400

    Update footer.js
    minor update to follow one of React Strict Mode's conventions: target="_blank" must go with rel="noreferrer"

--- src/footer.js
+++ src/footer.js
@@ -1,7 +1,7 @@
 const Footer = () => (
     <div className="footer">
         <div className="col">
-            <a href="https://github.com/jlulloaa/badbank/blob/master/LICENSE.md" target="_blank"> 
+            <a href="https://github.com/jlulloaa/badbank/blob/master/LICENSE.md" target="_blank" rel="noreferrer"> 
                 <img alt="GitHub" src="https://img.shields.io/github/license/jlulloaa/badbank?logo=github&style=social" />
             </a>
         </div>
